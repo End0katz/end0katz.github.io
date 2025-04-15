ea4b230756bef9f31814e392bee7a2e79b28bf7d
End0katz <endokatz@gmail.com>
2025-04-15 08:41:32 +0100

    Added additional images to the background (easter eggs yay)

--- javascript/bg_img.js
+++ javascript/bg_img.js
@@ -1,14 +1,18 @@
 // Configuration: Chance percentage for adding the animated-hueshift class
 const effectChance = 5;
 
-console.log('Loaded bg_img.js');
-
 const fixed = false;
-const test = '';
+const test = 'blur';
 
 const effects = [
     'hueshift',
     'bright'
+]
+
+const imageChance = 5;
+const images = [
+    '/images/gremlin.png',
+    '/images/java-logo.png'
 ]
 
 bg_img = `
@@ -23,6 +27,11 @@ document.body.appendChild(tempDiv.firstElementChild);
 
 // Set image to the <img> tag inside bg_img
 const image = document.querySelector('.bg-logo-img');
+
+// Random chance to use a different image
+if (Math.random() * 100 < imageChance) {
+    image.src = images[Math.floor(Math.random() * images.length)];
+}
 
 // Apply the animated-hueshift class if the condition is met
 if (Math.random() * 100 < effectChance) {
